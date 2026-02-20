a93217d84e6c14ded3bd2c61a3acba17ebf95157
Tejas <tejas.rajput@aspiresoftserv.com>
2026-02-20 10:23:59 +0530

    updation in index file

--- postgresql/src/db/db.ts
+++ postgresql/src/db/db.ts
@@ -12,6 +12,6 @@ export const query = async (text:string, params?: any[]) => {
   const start = Date.now()
   const res = await client.query(text, params)
   const duration = Date.now() - start
-  console.log('executed query', { text, duration, rows: res.rowCount })
+  console.log('executed query', { duration, rows: res.rowCount })
   return res
 }
