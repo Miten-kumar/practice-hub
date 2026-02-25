62065b212adc6cbf1e4523de6417952b29e9694a
Tejas <tejas.rajput@aspiresoftserv.com>
2026-02-25 16:04:26 +0530

    fix: PORT issue fixed

--- authentication-authorization/src/index.ts
+++ authentication-authorization/src/index.ts
@@ -19,6 +19,6 @@ AppDataSource.initialize()
 app.use(express.json());
 app.use("/api/users", userRoutes);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
 });
